55a6daaac489a61762e679f83f6a4ed2f15fe6c9
slang123123 <31445757+950821@users.noreply.github.com>
2025-11-04 18:32:57 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "我的微博客",
-	subtitle: "D这是描述",
+	title: "落雨无秋",
+	subtitle: "测试",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
